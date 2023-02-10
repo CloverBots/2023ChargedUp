519a08893beb3130bdaef9064a0856a9d2e831e0
MARTIKEL001 <66886359+MadMan310@users.noreply.github.com>
2023-02-09 18:52:26 -0800

    Add TelescopeCommand

---
+++ src/main/java/frc/robot/commands/TelescopeCommand.java
@@ -0,0 +1,34 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.TelescopeSubsystem;
+
+public class TelescopeCommand extends CommandBase {
+
+  TelescopeSubsystem telescopeSubsystem;
+
+  public final double UPPER_ENDPOINT = 10;
+  public final double LOWER_ENDPOINT = 0;
+
+  public TelescopeCommand(TelescopeSubsystem telescopeSubsystem) {
+    this.telescopeSubsystem = telescopeSubsystem;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
